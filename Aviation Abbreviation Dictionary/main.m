594ffc3b2a11ae844fbead35ec1a014a3eab4302
Adam Birdsall <adambirdsall@Adams-MacBook-Pro.local>
2014-06-24 19:18:47 -0700

    Initial Commit

---
+++ Aviation Abbreviation Dictionary/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Aviation Abbreviation Dictionary
+//
+//  Created by Adam Birdsall on 6/24/14.
+//  Copyright (c) 2014 Adam Birdsall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
